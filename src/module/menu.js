4ab741f96cc9e5e8091ebfe3a7b1afe6a4a8aaad
Nikita Polev <nikitko779@gmail.com>
2024-10-08 22:46:28 +0300

    исправил код

--- src/module/menu.js
+++ src/module/menu.js
@@ -1,20 +1,17 @@
 const menu = () => {
     const menuPanel = document.querySelector("menu");
     const body = document.querySelector("body");
-    
+
     const handlMenu = () => {
-        menuPanel.classList.toggle("active-menu"); 
+        menuPanel.classList.toggle("active-menu");
     };
 
     const toggleMenu = () => {
         body.addEventListener("click", (e) => {
-            console.log(e.target);
-            
+
             if (e.target.closest(".menu") || e.target.closest(".close-btn") || e.target.closest("ul>li>a")) {
                 handlMenu();
-            } else if (!e.target.closest('.active-menu')) {
-                console.log(e.target);
-                
+            } else if (!e.target.closest('.active-menu') && menuPanel.classList.contains('active-menu')) {
                 handlMenu();
             }
         });
