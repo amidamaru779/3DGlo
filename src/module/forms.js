ebcd9f58727e99f42eb94d9652d051e4ee29a4c5
Nikita Polev <nikitko779@gmail.com>
2024-10-11 20:35:15 +0300

    +

--- src/module/forms.js
+++ src/module/forms.js
@@ -1,34 +1,68 @@
 const forms = () => {
-    const regExpName = /[^а-я -]+/gi
-    const regExpEmail = /[^a-z@0-9-_.!~*']+/gi
-    const regExpPhone = /[^0-9()-]/
+    const regExpName = /^[-?!,.а-яА-ЯёЁ]{2,}$/
+    const regExpEmail = /^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/
+    const regExpPhone = /(\+7|8)[\s(]?(\d{3})[\s)]?(\d{3})[\s-]?(\d{2})[\s-]?(\d{2})/g
+    const reExpMessage = /^[-?!,.а-яА-ЯёЁ\s]/
 
     const userName = document.querySelectorAll('.form-name')
     const userEmail = document.querySelectorAll('.form-email')
     const userPhone = document.querySelectorAll('.form-phone')
+    const userMessage = document.querySelector(".mess")
 
+
+    const isUserDataValid = (regExp, data) => {
+        return regExp.test(data)
+    }
     const userValid = () => {
 
         userName.forEach(item => {
             item.addEventListener('input', (e) => {
-                e.target.value = e.target.value.replace(regExpName, '')
+                e.preventDefault()
+                if (isUserDataValid(regExpName, item.value)) {
+                    item.classList.add('success')
+                } else {
+                    item.classList.remove('success')
+                }
             })
         })
 
         userEmail.forEach(item => {
             item.addEventListener('input', (e) => {
-                e.target.value = e.target.value.replace(regExpEmail, '')
+                e.preventDefault()
+                if (item.value.trim() === '') {
+                    item.setCustomValidity('Заполните это поле');
+                    item.classList.remove('success');
+                } else if (isUserDataValid(regExpEmail, item.value)) {
+                    item.setCustomValidity('');
+                    item.classList.add('success');
+                } else {
+                    item.classList.remove('success');
+                }
             })
         })
 
         userPhone.forEach(item => {
             item.addEventListener('input', (e) => {
-                e.target.value = e.target.value.replace(regExpPhone, '')
+                e.preventDefault()
+                if (isUserDataValid(regExpPhone, item.value)) {
+                    item.classList.add('success')
+                } else {
+                    item.classList.remove('success')
+                }
             })
         })
-
+        userMessage.addEventListener('input', (e) => {
+            e.preventDefault()
+            if (isUserDataValid(reExpMessage, userMessage.value)) {
+                userMessage.classList.add('success')
+            } else {
+                userMessage.classList.remove('success')
+            }
+        })
     }
     userValid()
-};
+}
+
+
 
 export default forms;
--- src/module/sendForm.js
+++ src/module/sendForm.js
@@ -3,104 +3,108 @@ const sendForm = ({
     someElem = []
 }) => {
     const form = document.getElementById(formId);
-    console.log(form);
 
-    const statusBlock = document.createElement('div')
-    const loadText = 'Загрузка...'
-    const errorText = 'Ошибка...'
-    const successText = 'Спасибо! Наш менеджер с Вами свяжется!'
 
-    const validateInput = (input) => {
-        const name = input.name
-        const value = input.value
+    const loadText = "Загрузка...";
+    const errorText = "Ошибка...";
+    const successText = "Спасибо! Наш менеджер с Вами свяжется";
 
-        if (name === 'user_phone') {
+    const getBlock = () => {
+        return form.querySelector(".status");
+    };
 
-            return /^[0-9+\-\(\) ]*$/.test(value)
-        } else if (name === 'user_name') {
+    const getstatusBlock = (statusText) => {
+        const statusBlock = getBlock();
+        statusBlock.textContent = statusText;
+    };
 
-            return /^[А-Яа-яЁё\s]*$/.test(value)
-        } else if (name === 'user_message') {
-            return /^[А-Яа-яЁё0-9\.,!?;:\s]*$/.test(value)
-        }
-        return true
-    }
+    const createStatusBlock = (form) => {
+        const statusBlock = document.createElement("div");
+        statusBlock.classList.add("status");
+        statusBlock.style.color = "red";
+        form.append(statusBlock);
+    };
 
-    const validate = (inputs) => {
-        let isValid = true
-        inputs.forEach((input) => {
-            if (!validateInput(input)) {
-                isValid = false;
-                input.classList.remove('success')
-                input.classList.add('error')
-            } else {
-                input.classList.remove('error')
-                input.classList.add('success')
+    const validate = (list) => {
+        let success = true;
+        list.forEach((input) => {
+            console.log(input.value, input.classList.contains("success"))
+            if (!input.classList.contains("success")) {
+                success = false;
             }
-        })
-        return isValid
-    }
+        });
+        return success;
+    };
 
     const sendData = (data) => {
-        return fetch('https://jsonplaceholder.typicode.com/posts', {
-            method: 'POST',
+        return fetch("https://jsonplaceholder.typicode.com/posts", {
+            method: "POST",
             body: JSON.stringify(data),
             headers: {
-                "Content-Type": "application/json"
-            }
-        }).then(res => res.json())
-    }
+                "Content-Type": "application/json",
+            },
+        }).then((res) => res.json());
+    };
 
     const submitForm = () => {
-        const formElements = form.querySelectorAll('input')
-        const formData = new FormData(form)
-        const formBody = {}
+        const formData = new FormData(form);
+        const formBody = {};
 
-        statusBlock.textContent = loadText
-        form.append(statusBlock)
+        createStatusBlock(form);
+        getstatusBlock(loadText);
 
         formData.forEach((val, key) => {
-            formBody[key] = val
-        })
+            formBody[key] = val;
+        });
 
-        someElem.forEach(elem => {
-            const element = document.getElementById(elem.id);
-            console.log(element)
-
-            if (elem.type === 'block') {
-                formBody[elem.id] = element.textContent
-            } else if (elem.type === 'input') {
-                formBody[elem.id] = elem.value
+        someElem.forEach((element) => {
+            const elem = document.getElementById(element.id);
+            if (element.type === "block") {
+                formBody[element.id] = elem.textContent;
+            } else if (element.type === "input") {
+                formBody[element.id] = elem.value;
             }
-        })
+        });
+
+        const formElements = form.querySelectorAll("input");
+        console.log(formElements);
+
+
 
         if (validate(formElements)) {
             sendData(formBody)
-                .then(data => {
-                    statusBlock.textContent = successText;
-                    formElements.forEach(input => {
-                        input.value = ''
+                .then((data) => {
+                    getstatusBlock(successText);
+
+                    formElements.forEach((input) => {
+                        input.value = "";
                     });
+                    setTimeout(() => {
+                        getBlock().remove();
+                    }, 2000);
                 })
-                .catch(error => {
-                    statusBlock.textContent = errorText
+                .catch((error) => {
+                    getstatusBlock(errorText);
                 });
         } else {
-            alert('Данные не валидны!!')
+            getstatusBlock(errorText);
+            setTimeout(() => {
+                getBlock().remove();
+            }, 2000);
         }
-    }
+    };
 
     try {
         if (!form) {
-            throw new Error("Вверни форму на место, пожалуйста")
+            throw new Error("Вверни форму на место, пожалуйста");
         }
-        form.addEventListener('submit', (e) => {
-            e.preventDefault()
-            submitForm()
-        })
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+            submitForm();
+        });
     } catch (error) {
-        console.log(error.message)
+        console.log(error.message);
     }
-}
+};
 
 export default sendForm;
